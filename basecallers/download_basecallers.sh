20d64035528ba725864ecd26ddf07b6747207003
elonlit <elonlit@gmail.com>
2024-09-28 18:27:48 -0400

    Streamline benchmarking process

---
+++ basecallers/download_basecallers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script downloads all of the necessary basecallers for the benchmarking process
+# Run this script inside the basecallers/ directory
+
+./download_dorado.sh
+./download_gcrtcall.sh
+./download_guppy.sh
